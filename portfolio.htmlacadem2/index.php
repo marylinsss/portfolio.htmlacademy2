b958c629a088db0b8fe2d66438254db0fb71bb7b
marylinsss <71058839+marylinsss@users.noreply.github.com>
2020-12-08 17:42:15 +0300

    portfolio HT

---
+++ portfolio.htmlacadem2/index.php
@@ -0,0 +1,15 @@
+<?php
+$title = 'Верстальщик-фронтендер';
+$name = 'Маша';
+$image = 'img/unknown-raccoon.svg';
+$email = 'msavinko@gmail.com';
+$phone = '+7(926)658-71-82';
+
+require('components/header.php');
+require('components/about.php');
+require('components/status.php');
+require('components/photos.php');
+//require('components/news.php');
+require('components/quotes.php');
+require('components/contacts.php');
+require('components/footer.php');
